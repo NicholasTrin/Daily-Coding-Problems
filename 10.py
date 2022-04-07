8f6453da507a594182ba6a3a84654ab3919d19cf
Nicholas Trindade <nmd.trindade@gmail.com>
2022-04-06 19:57:53 -0700

    Added Challenges

---
+++ 10.py
@@ -0,0 +1,51 @@
+from time import sleep, time
+import threading
+
+class Scheduler:
+    def __init__(self):
+        self.to_call = []
+        self.scheduler_thread = threading.Thread(target=self._initiate)
+        self.scheduler_thread.start()
+
+
+    def _initiate(self):
+        while True:
+            curr_time = time()*1000
+            for i in self.to_call:
+                func, time_due, args = i
+                if curr_time >= time_due:
+                    func(*args)
+                    self.to_call.remove(i)
+                    self.to_call = [to_call for to_call in self.to_call if to_call]
+
+    def add_call(self,func,delay,*args):
+        self.to_call.append([func, delay + time()*1000, args])
+
+
+def random_call(a,b,c):
+    print(time())
+    print(a+b+c)
+
+
+if __name__ == '__main__':
+    sched = Scheduler()
+    sched.add_call(random_call,1000,1,2,3)
+    sched.add_call(random_call, 1000, 1, 2, 3)
+    sched.add_call(random_call, 2000, 1, 2, 3)
+    sched.add_call(random_call, 4000, 1, 2, 3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+
--- README.md
+++ README.md
@@ -6,5 +6,7 @@ Practice material for proficiency
 4. Completed
 5. Completed
 6. WIP, needs testing with some form of python pointers. Will look into memory space allocations.
-7. WIP
+7. Completed
 8. Completed
+9. WIP
+10. Completed
